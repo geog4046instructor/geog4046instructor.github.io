99d3025adf7ed41a1c5a112ea516c24d8be5431f
geog4046instructor <geog4046instructor@gmail.com>
2021-03-13 13:25:32 -0600

    Update map1.js

--- map1/map1.js
+++ map1/map1.js
@@ -1,5 +1,11 @@
 var map = L.map('map').setView([38, -95], 4);
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
-var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
+var radarDisplayOptions = {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+};
+var radarUrl = 'http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
