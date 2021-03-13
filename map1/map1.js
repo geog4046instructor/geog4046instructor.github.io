1a979e037a573a7cf5126c50e8a59e10444fbd9f
geog4046instructor <geog4046instructor@gmail.com>
2021-03-13 12:50:21 -0600

    Update map1.js

--- map1/map1.js
+++ map1/map1.js
@@ -1,3 +1,5 @@
 var map = L.map('map').setView([38, -95], 4);
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
+var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
+var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
